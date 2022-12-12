5f0cf074f6a47b077485c5b46351ac0ac324f612
Teehxw <112659668+Teehxw@users.noreply.github.com>
2022-12-12 14:47:36 -0700

    gfhfgh

--- js/main.js
+++ js/main.js
@@ -4,11 +4,25 @@
 let signInBtn = document.getElementById('sign-in-btn');
 let signUpBtn = document.getElementById('sign-up-btn');
 
+
+let confirmInput = document.getElementById('confpass');
+
+//Array
+let member = loadMember();
+
 // SIGN UP BTN CLICKED
 signUpBtn.addEventListener('click', signUpHandler);
 
 function signUpHandler() {
-  console.log('Sign Up Btn Clicked');
+  let membUser = document.getElementById('user');
+  let membPass = document.getElementById('pass');
+  member.push(newMember(membUser, membPass));
+  saveMember();
+  // userInput.value= "";
+  // passInput.value= "";
+
+
+  alert("Sign Up Successful");
 }
 
 // SIGN IN BTN CLICKED
@@ -16,4 +30,23 @@ signInBtn.addEventListener('click', signInHandler);
 
 function signInHandler() {
   console.log('Sign In Btn Clicked');
+}
+
+function saveMember(){
+  localStorage.setItem('member', JSON.stringify(member));
+}
+
+function loadMember(){
+  let memberStr = localStorage.getItem('member');
+  return JSON.parse(memberStr) ?? [];
+}
+  
+function newMember(memberUser, memberPass ) {//, conf{
+  return {
+    membUser: memberUser,
+    membPass: memberPass, 
+    // confirmPass: contactNumbers,  
+    completed: false
+    
+  };
 }
